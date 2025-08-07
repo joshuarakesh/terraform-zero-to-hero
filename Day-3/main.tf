f28175cbee160c6759fab7cee79555b3b336425d
joshua <joshuarakesh@gmail.com>
2025-08-07 13:31:20 +0530

    Update main.tf
    added code to display output ip  and instance name

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+    source = "./modules/ec2_instance"
+    ami_value = "ami-020cba7c55df1f615"
+    instance_type_value = "t2.micro"
+    subnet_id_value = "subnet-053c34acfaeb0e55e"
+    ec2_name_value = "Terraform-EC2-Day3"
+}
+
+
+output "ec2_public_ip" {
+  value = module.ec2-instance.public-ip-address
+}
+
+output "ec2_instance_name" {
+  value = module.ec2-instance.instance_name
 }
