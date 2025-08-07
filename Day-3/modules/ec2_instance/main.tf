610539f6b57959aadb8ee31f4035826a3edc63ce
joshua <joshuarakesh@gmail.com>
2025-08-07 13:27:31 +0530

    Update main.tf
    code for instance name added

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my_ec2_instance_day3" {
     ami = var.ami_value
     instance_type = var.instance_type_value
-    subnet_id = var.subnet_id_value
+        tags = {
+        Name = var.ec2_name_value
+    }
 }
