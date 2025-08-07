9f77fb1f41882cc014f0205bc7aada3b006548f8
joshua <joshuarakesh@gmail.com>
2025-08-07 13:28:24 +0530

    Update outputs.tf
    added instance name

--- Day-3/modules/ec2_instance/outputs.tf
+++ Day-3/modules/ec2_instance/outputs.tf
@@ -1,3 +1,7 @@
 output "public-ip-address" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.my_ec2_instance_day3.public_ip
+}
+
+output "instance_name" {
+  value = aws_instance.my_ec2_instance_day3.tags["Name"]
 }
