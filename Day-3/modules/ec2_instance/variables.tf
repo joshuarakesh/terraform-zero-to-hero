ae93323eb8af07b2cc71c93074c040dfbc6c7190
joshua <joshuarakesh@gmail.com>
2025-08-07 13:29:17 +0530

    Update variables.tf
    instance name added

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -6,6 +6,7 @@ variable "instance_type_value" {
     description = "value for instance_type"
 }
 
-variable "subnet_id_value" {
-    description = "value for the subnet_id"
+variable "ec2_name_value" {
+  description = "Name tag for the ec2 instance"
+  type        = string
 }
